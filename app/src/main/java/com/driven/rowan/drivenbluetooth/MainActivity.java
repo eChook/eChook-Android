158d7eea05e90369dfc7a9532efd9b2e7ffd148b
RJPGriffin <rowangee@gmail.com>
2015-03-02 23:34:55 +0000

    Added receiver code

--- app/src/main/java/com/driven/rowan/drivenbluetooth/MainActivity.java
+++ app/src/main/java/com/driven/rowan/drivenbluetooth/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends ActionBarActivity {
 
     TextView myLabel;
     EditText myTextbox;
+    EditText myIncoming;
     BluetoothAdapter mBluetoothAdapter;
     BluetoothSocket mmSocket;
     BluetoothDevice mmDevice;
@@ -42,6 +43,8 @@ public class MainActivity extends ActionBarActivity {
     int readBufferPosition;
     int counter;
     volatile boolean stopWorker;
+    boolean deviceConnected = false;
+    boolean matchingDeviceFound = false;
 
     String TAG = "DBDebug";
 
@@ -55,6 +58,7 @@ public class MainActivity extends ActionBarActivity {
         Button closeButton = (Button)findViewById(R.id.close);
         myLabel = (TextView)findViewById(R.id.label);
         myTextbox = (EditText)findViewById(R.id.entry);
+        myIncoming = (EditText)findViewById(R.id.incoming);
 
         //Open Button
         openButton.setOnClickListener(new View.OnClickListener() {
@@ -93,9 +97,14 @@ public class MainActivity extends ActionBarActivity {
 
 
     void findBT() {
+
+        stopWorker = true;
         Log.d( TAG, "Entering FindBT");
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         Log.d(TAG, "FindBT before If");
+
+        mBluetoothAdapter.cancelDiscovery();
+
         if(mBluetoothAdapter == null) {
             Log.d(TAG, "Entering BT Adapter == null");
             myLabel.setText("No bluetooth adapter available");
@@ -124,7 +133,7 @@ public class MainActivity extends ActionBarActivity {
                 if(device.getName().equals("RNBT-62D8")) { //TODO Handle scenario where device is not paired to phone
 
                     Log.d(TAG, "Device Matching Name Found");
-
+                    matchingDeviceFound = true;
                     mmDevice = device;
                     break;
                 }
@@ -135,30 +144,36 @@ public class MainActivity extends ActionBarActivity {
     }
 
     void openBT() throws IOException {
+        if(matchingDeviceFound) {
+            Log.d(TAG, "Attempting to open BT Device");
 
-        Log.d(TAG, "Attempting to open BT Device");
+            UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); //Standard //SerialPortService ID
+            Log.d(TAG, "Connecting Socket Service to device");
+            mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
+            Log.d(TAG, "Connecting to socket ");
+            mmSocket.connect(); //TODO set timer on mmSocket.Connect - else blocks thread when failing to connect
+            Log.d(TAG, "Setting Output Stream");
+            mmOutputStream = mmSocket.getOutputStream();
+            Log.d(TAG, "Setting Input Stream");
+            mmInputStream = mmSocket.getInputStream();
+            Log.d(TAG, "Starting to Listen");
+            beginListenForData();
+            deviceConnected = true;
+            myLabel.setText("Bluetooth Opened");
+        }else {
+            Log.d(TAG, "No matching device found to open");
+        }
 
-        UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); //Standard //SerialPortService ID
-        Log.d(TAG, "Connecting Socket Service to device");
-        mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
-        Log.d(TAG, "Connecting to socket ");
-        mmSocket.connect(); //TODO set timer on mmSocket.Connect - else blocks thread when failing to connect
-        Log.d(TAG, "Setting Output Stream");
-        mmOutputStream = mmSocket.getOutputStream();
-        Log.d(TAG, "Setting Input Stream");
-        mmInputStream = mmSocket.getInputStream();
-        Log.d(TAG, "Starting to Listen");
-        beginListenForData();
-        myLabel.setText("Bluetooth Opened");
+
     }
 
     void beginListenForData() {
         final Handler handler = new Handler();
-        final byte delimiter = 10; //This is the ASCII code for a newline character
+        final byte delimiter = 92; //10 is the ASCII code for a newline character
 
         stopWorker = false;
         readBufferPosition = 0;
-        readBuffer = new byte[1024];
+        readBuffer = new byte[2048];
         workerThread = new Thread(new Runnable() {
             public void run() {
                 while(!Thread.currentThread().isInterrupted() && !stopWorker) {
@@ -177,7 +192,7 @@ public class MainActivity extends ActionBarActivity {
 
                                     handler.post(new Runnable() {
                                         public void run() {
-                                            myLabel.setText(data);
+                                            myIncoming.append(data+"\n");
                                         }
                                     });
                                 }
@@ -207,10 +222,14 @@ public class MainActivity extends ActionBarActivity {
 
     void closeBT() throws IOException {
         stopWorker = true;
-        mmOutputStream.close();
-        mmInputStream.close();
-        mmSocket.close();
-        myLabel.setText("Bluetooth Closed");
+
+        if (deviceConnected) {
+            mmOutputStream.close();
+            mmInputStream.close();
+            mmSocket.close();
+            myLabel.setText("Bluetooth Closed");
+            deviceConnected = false;
+        }
     }
 
     private void showMessage(String theMsg) {
